8197425a23d29b2026de45fa9a197cc64c41f3d2
Adebola <bfagbowore@gmail.com>
2022-10-29 06:00:22 +0100

    setup project, design threads page using json data

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <page-home />
+  </div>
+</template>
+
+<script>
+import PageHome from "./components/PageHome.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageHome,
+  },
+};
+</script>
+
+<style>
+@import "assets/style.css";
+</style>
